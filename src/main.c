27735086d7bf38949062736a97af2ca6e2a85afe
Alexander Dahl <post@lespocky.de>
2013-08-02 15:48:21 +0200

    test on breadboard with different uC

--- src/main.c
+++ src/main.c
@@ -1,23 +1,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define WAIT_MS 100
+#define WAIT_MS 500
 
 int main (void) {
-//    int t = 0;
+    int t = 0;
 
-    DDRB = (1 << DDB1) | (1 << DDB2) | (1 << DDB0);
+    DDRB = (1 << DDB0);
     PORTB = 0;
 
-//    while ( 1 ) {
-//        PORTB = (unsigned char) t++ & 0x1f;
-//        _delay_ms( WAIT_MS );
-//    }
-    // PORTB = (1 << PORTB3) | (1 << PORTB1) | (1 << PORTB4);
-    // PORTB = (1 << PORTB3);
-    // PORTB = (1 << PORTB1);
-    PORTB = 0xFF;
-//    PORTB = ~(1 << PORTB0);
+    while ( 1 ) {
+        PORTB = (unsigned char) t++ & 0x01;
+        _delay_ms( WAIT_MS );
+    }
 
     return 0;
 }
